680a8ca23b8f41a5504282b42c0ccbc4024b98df
wojcho <wojhoo3@gmail.com>
2026-04-08 14:48:58 +0200

    Change @Component to @Service because of noticing in task that it should be @Service, though functionally there should be not much difference as both add it to be scanned by Spring

--- 3_spring/src/main/kotlin/com/example/authorized_hello/EagerAuthorization.kt
+++ 3_spring/src/main/kotlin/com/example/authorized_hello/EagerAuthorization.kt
@@ -1,9 +1,9 @@
 package com.example.authorized_hello
 
-import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
 
 // https://www.baeldung.com/kotlin/singleton-classes
-@Component
+@Service
 object EagerAuthorization {
   fun isAuthorized(username: String, password: String): Boolean {
     return username == "premier" && password == "admin1";
