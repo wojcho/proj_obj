346bf6c44234d5715c4eb60a7e487b820ce0b789
wojcho <wojhoo3@gmail.com>
2026-04-08 14:40:31 +0200

    Added dependency injection using Autowired

--- 3_spring/src/main/kotlin/com/example/authorized_hello/EagerAuthorization.kt
+++ 3_spring/src/main/kotlin/com/example/authorized_hello/EagerAuthorization.kt
@@ -1,4 +1,9 @@
+package com.example.authorized_hello
+
+import org.springframework.stereotype.Component
+
 // https://www.baeldung.com/kotlin/singleton-classes
+@Component
 object EagerAuthorization {
   fun isAuthorized(username: String, password: String): Boolean {
     return username == "premier" && password == "admin1";
--- 3_spring/src/main/kotlin/com/example/authorized_hello/SecretController.kt
+++ 3_spring/src/main/kotlin/com/example/authorized_hello/SecretController.kt
@@ -8,19 +8,20 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.server.ResponseStatusException
 import org.springframework.http.HttpStatus
+import org.springframework.beans.factory.annotation.Autowired
 
 private val userHelloSecrets = mapOf("premier" to "Handling of this information requires utmost secrecy")
 
 @RestController
-class SecretController {
+class SecretController @Autowired constructor(private val authorization: EagerAuthorization) {
 
   private val counter = AtomicLong()
 
   @GetMapping("/secret/{username}")
   fun secret(@PathVariable("username") username: String, @RequestParam("password") password: String): Hello {
-    val secretOfUser = userHelloSecrets[username] ?: throw ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unrecognized username")
-    if (!EagerAuthorization.isAuthorized(username, password)) {
-      throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Credentials mismatch")
+    val secretOfUser = userHelloSecrets[username] ?: throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Unrecognized username")
+    if (!authorization.isAuthorized(username, password)) {
+      throw ResponseStatusException(HttpStatus.UNAUTHORIZED, "Credentials mismatch")
     }
     return Hello(counter.incrementAndGet(), secretOfUser)
   }
