b667250ca2fd1196bf8a52b0e777f666459d4e2a
wojcho <wojhoo3@gmail.com>
2026-05-11 10:41:48 +0200

    Prepared a few cities with preset names

--- 4_echo/server.go
+++ 4_echo/server.go
@@ -15,6 +15,13 @@ func main() {
 
 	proxy := NewWeatherProxy()
 
+	cities := map[string][2]float64{
+		"krakow":    {50.0646, 19.9449},
+		"warszawa":  {52.2277, 21.0018},
+		"praga":     {50.0875, 14.4212},
+		"budapeszt": {47.5072, 19.0450},
+	}
+
 	e.GET("/weather", func(c *echo.Context) error {
 		latStr := c.QueryParam("latitude")
 		lonStr := c.QueryParam("longitude")
@@ -28,6 +35,20 @@ func main() {
 			return c.String(http.StatusBadRequest, "incorrect longitude: "+err.Error())
 		}
 
+		w, err := proxy.FetchWeatherNow(lat, lon)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "failed to fetch weather: "+err.Error())
+		}
+		return c.JSON(http.StatusOK, w.ToDto())
+	})
+
+	e.GET("/weather/:city", func(c *echo.Context) error {
+		city := c.Param("city")
+		coords, ok := cities[city]
+		if !ok {
+			return c.String(http.StatusNotFound, "unknown city")
+		}
+		lat, lon := coords[0], coords[1]
 		w, err := proxy.FetchWeatherNow(lat, lon)
 		if err != nil {
 			return c.String(http.StatusInternalServerError, "failed to fetch weather: "+err.Error())
